366440977364fc7e9ec4ef00343aa43430750bba
PhoenixGS <thestarrydream@gmail.com>
2018-04-24 10:00:35 +0800

    Update

--- 958/F3.cpp
+++ 958/F3.cpp
@@ -1,4 +1,39 @@
 #include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+struct Complex
+{
+	double real, imag;
+
+	Complex(){}
+
+	Complex(double _real, double _imag): real(_real), imag(_imag){}
+};
+
+const int M = 1009;
+const double pi = acos(-1);
+int n, m, k;
+int x[600000];
+int* point[600000];
+int v[600000];
+Complex xx[600000], yy[600000];
+int rev[600000];
+
+Complex operator + (Complex x, Complex y)
+{
+	return Complex(x.real + y.real, x.imag + y.imag);
+}
+
+Complex operator - (Complex x, Complex y)
+{
+	return Complex(x.real - y.real, x.imag - y.imag);
+}
+
+Complex operator * (Complex x, Complex y)
+{
+	return Complex(x.real * y.real - x.imag * y.imag, x.real * y.imag + x.imag * y.real);
+}
 
 void getrev(int bit)
 {
@@ -8,11 +43,30 @@ void getrev(int bit)
 	}
 }
 
-void FFT(int *x, int n, int f)
+void FFT(Complex *x, int n, int f)
 {
 	for (int i = 1; i < n; i <<= 1)
 	{
-
+		Complex wn(cos(pi / i), f * sin(pi / i));
+		for (int j = 0; j < n; j += (i << 1))
+		{
+			Complex w(1, 0);
+			for (int k = 0; k < i; k++, w = w * wn)
+			{
+				Complex xx = x[j + k];
+				Complex yy = w * x[j + k + i];
+				x[j + k] = xx + yy;
+				x[j + k + i] = xx - yy;
+			}
+		}
+	}
+	if (f == -1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			x[i].real /= n;
+			x[i].imag /= n;
+		}
 	}
 }
 
@@ -25,22 +79,28 @@ void solve(int l, int r)
 	int mid = (l + r) >> 1;
 	solve(l, mid);
 	solve(mid + 1, r);
-	int n = point[mid + 1] - v[l];
-	int m = point[r + 1] - v[mid + 1];
+	int n = point[mid + 1] - point[l];
+	int m = point[r + 1] - point[mid + 1];
 	int s = 1, bit = 9;
 	for (s = 1, bit = 0; s <= n + m; s <<= 1, bit++);
 	for (int i = 0; i < s; i++)
 	{
-		xx[i] = yy[i] = 0;
+		xx[i] = yy[i] = Complex(0, 0);
 	}
+	puts("X");
 	for (int i = 0; i < n; i++)
 	{
-		xx[i] = (point[l])[i];
+		printf("%d ", (point[l])[i]);
+		xx[i] = Complex((point[l])[i], 0);
 	}
+	puts("");
+	puts("Y");
 	for (int i = 0; i < m; i++)
 	{
-		yy[i] = (point[mid + 1])[i];
+		printf("%d ", (point[mid + 1])[i]);
+		yy[i] = Complex((point[mid + 1])[i], 0);
 	}
+	puts("");
 	getrev(bit);
 	FFT(xx, s, 1);
 	FFT(yy, s, 1);
@@ -51,7 +111,7 @@ void solve(int l, int r)
 	FFT(xx, s, -1);
 	for (int i = 0; i < n; i++)
 	{
-		(point[l])[i] = (long long)(xx[i] + 0.5) % M;
+		(point[l])[i] = (long long)(xx[i].real + 0.5) % M;
 	}
 }
 
@@ -65,7 +125,7 @@ int main()
 	std::sort(x + 1, x + n + 1);
 	int cnt = 1;
 	int last = 0;
-	s = 0;
+	int s = 0;
 	for (int i = 2; i <= n; i++)
 	{
 		if (x[i] != x[i - 1])
@@ -94,4 +154,6 @@ int main()
 	point[s + 1] = point[s] + 1;
 	cnt = 1;
 	solve(1, s);
+	printf("%d\n", (point[1])[k]);
+	return 0;
 }
